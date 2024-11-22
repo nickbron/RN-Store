827aa0b371a071d62e94cd3d2b7dd55baae5acd5
nick_bron <nick_bron@ukr.net>
2024-11-22 18:19:34 +0200

    create app

---
+++ src/app/(shop)/_layout.tsx
@@ -0,0 +1,59 @@
+import { Tabs } from "expo-router";
+import { Platform, StatusBar, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FontAwesome } from "@expo/vector-icons";
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+  color: string;
+}) {
+  return <FontAwesome size={24} {...props} style={{ color: "#1BC464" }} />;
+}
+
+const TabsLayout = () => {
+  return (
+    // <SafeAreaView edges={["top"]} style={styles.safeArea}>
+    <SafeAreaView edges={["top"]} style={styles.AndroidSafeArea}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#1BC464",
+          tabBarInactiveTintColor: "gray",
+          tabBarLabelStyle: { fontSize: 16 },
+          tabBarStyle: {
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+          },
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "shop",
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name="shopping-cart" />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="orders"
+          options={{
+            title: "Orders",
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name="book" />;
+            },
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
+  );
+};
+
+export default TabsLayout;
+
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
