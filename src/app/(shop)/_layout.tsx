890b5fdd5d8f36f6e0f6bbadc943e4548651f0c0
nick_bron <nick_bron@ukr.net>
2024-11-22 21:05:58 +0200

    build Home page

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -12,7 +12,6 @@ function TabBarIcon(props: {
 
 const TabsLayout = () => {
   return (
-    // <SafeAreaView edges={["top"]} style={styles.safeArea}>
     <SafeAreaView edges={["top"]} style={styles.AndroidSafeArea}>
       <Tabs
         screenOptions={{
--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Cart() {
   return (
     <View>
-      <Text>Cart</Text>
+      <Text>Cart1</Text>
     </View>
   );
 }
